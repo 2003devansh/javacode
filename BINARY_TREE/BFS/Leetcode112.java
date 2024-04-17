39cb7318ede1de4f215305ab389377b130c3ec67
2003devansh <faujdardevansh@gmail.com>
2024-04-17 22:17:54 +0530

    commit

---
+++ BINARY_TREE/BFS/Leetcode112.java
@@ -0,0 +1,39 @@
+package BINARY_TREE.BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Leetcode112 {
+    public static void main(String[] args) {
+        
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    
+        TreeNode() {}
+    
+        TreeNode(int val) {
+            this.val = val;
+        }
+    
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false ;
+        }
+        Queue<TreeNode> queue = new LinkedList<>(); 
+        queue.offer(root) ;
+
+        
+
+    }
+    
+}
