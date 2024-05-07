9e663208b353f811a15a59b18a759736f27db196
2003devansh <faujdardevansh@gmail.com>
2024-05-07 16:40:47 +0530

    commit

---
+++ leetcode/ques2.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+public class ques2 {
+    public static void main(String[] args) {
+        
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+       
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0) ;
+        ListNode ptr = result ;
+        int carry = 0 ; 
+        while(l1 != null || l2 != null){
+            int sum = 0 + carry ;
+
+            if( l1 != null){
+                sum += l1.val ;
+                l1 = l1.next ;
+            }
+            if(l2 != null){
+                sum += l2.val ;
+                l2 = l2.next ;
+            }
+
+            // lets say number add hoke bna 10 
+            // toh / 1 ko alg kar dega 
+            // aur % 0 ko alg kar dega
+
+            carry = sum /10 ; 
+            sum = sum%10 ;
+            ptr.next = new ListNode(sum) ;
+            // fir  hum yha pe last digit ko new listnode mai store kar lenege 
+            ptr = ptr.next ;
+        }
+
+        if(carry == 1){
+            ptr.next = new ListNode(1);
+        }
+        return result.next ; 
+    }
+    
+}
