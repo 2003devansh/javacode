e61c3ef025323a1b3a2cd61d9f48f8ca34420c08
2003devansh <faujdardevansh@gmail.com>
2024-12-23 13:28:44 +0530

    g

---
+++ randomjavaprograme/left_rotate_to_d_place.java
@@ -0,0 +1,16 @@
+package randomjavaprograme;
+
+import stack.stackmain;
+
+public class left_rotate_to_d_place {
+
+    public static void main(String[] args) {
+        
+    }
+
+    static int Left_rotation(int[] arr, int d){
+          for(int i = d ; i < n ; i++){
+            a[i-]
+          }
+    }
+}
