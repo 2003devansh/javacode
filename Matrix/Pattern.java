578ae6170431abc36d13e1b736eda0847d64a31a
2003devansh <faujdardevansh@gmail.com>
2025-01-27 15:09:36 +0530

    commit changes

---
+++ Matrix/Pattern.java
@@ -0,0 +1,19 @@
+package Matrix;
+
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt() ;
+            for(int i = 0 ;i < n ; i++){
+                for(int j = 0 ; j < n ; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        
+    }
+
+}
