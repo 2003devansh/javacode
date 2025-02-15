9ddc2facbd1e8c24ccf6520e44c609af87fbbaf3
2003devansh <faujdardevansh@gmail.com>
2025-02-15 22:41:25 +0530

    add: implement main method to demonstrate recursive Knapsack solution

--- Dynamic_programming/Knapsack_rec.java
+++ Dynamic_programming/Knapsack_rec.java
@@ -1,21 +1,31 @@
 package Dynamic_programming;
 
 public class Knapsack_rec {
+    
     public static void main(String[] args) {
+        int[] weight = {1, 2, 3, 5};
+        int[] val = {10, 20, 30, 50};
+        int capacity = 6;
+        int n = weight.length;
+
+        Knapsack_rec knapsack = new Knapsack_rec();
+        int maxProfit = knapsack.max_prof_knapsack(weight, val, capacity, n);
         
+        System.out.println("Maximum Profit: " + maxProfit);
     }
 
-    public int max_prof_knapsack(int[] weight, int[] val, int capacity, int n){
-        if(n == 0 || capacity == 0){
-            return 0 ;
+    public int max_prof_knapsack(int[] weight, int[] val, int capacity, int n) {
+        if (n == 0 || capacity == 0) {
+            return 0;
         }
 
-        if(weight[n-1] <= capacity){
-            return Math.max(val[n-1] + max_prof_knapsack(weight, val, capacity - weight[n-1], n-1), max_prof_knapsack(weight, val, capacity, n-1));
-        }else{
-            return max_prof_knapsack(weight, val, capacity, n-1);
+        if (weight[n - 1] <= capacity) {
+            return Math.max(
+                val[n - 1] + max_prof_knapsack(weight, val, capacity - weight[n - 1], n - 1),
+                max_prof_knapsack(weight, val, capacity, n - 1)
+            );
+        } else {
+            return max_prof_knapsack(weight, val, capacity, n - 1);
         }
-            
     }
-        
 }
